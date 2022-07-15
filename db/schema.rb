078358d3a71d397024e05935974a1119119f7eab
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-07-15 13:22:19 +0200

    Added image column to beers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_190109) do
+ActiveRecord::Schema.define(version: 2022_07_15_112112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_07_14_190109) do
     t.string "food", default: [], array: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
