ac60ef3fc12b2a592ba433153c8a9b7645738c38
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-07-15 16:47:46 +0200

    Stored beers from API call. Saved default image to images directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+def punk_api(page)
+  url = "https://api.punkapi.com/v2/beers?page=#{page}&per_page=80"
+  response = URI.open(url).read
+  JSON.parse(response)
+end
+
+def save_beers(result)
+  result.each do |beer|
+    p Beer.create(name: beer['name'], abv: beer['abv'],
+                  description: beer['description'], food: beer['food_pairing'],
+                  image: beer['image_url'])
+  end
+end
+
+def beers
+  page = 1
+  limit = 80
+  result = punk_api(page)
+  while result.length == limit
+    save_beers(result)
+    page += 1
+    result = punk_api(page)
+  end
+  save_beers(result)
+end
+
+beers
