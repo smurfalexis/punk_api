a9acaf59ba6ff3a5dd085d92331fca7f60353042
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-07-16 13:29:48 +0200

    Created tests for 'search beers'

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,12 @@
 class BeersController < ApplicationController
   def index
+    if params[:search] && params[:search][:keyword].present?
+      @key_word = params[:search][:keyword]
+      @beers = Beer.search_by_keyword(params[:search][:keyword])
+      @response = @beers.empty? ? "The beer you're looking for hasn't been brewed yet!" : 'This is what we found'
+    else
+      @beers = []
+      @response = "It seems like you didn't type anything. Try typing in a beer"
+    end
   end
 end
