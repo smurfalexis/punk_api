47388eabe59c41f2f4399f03e264d6705992b2b7
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-07-16 16:26:10 +0200

    Created tests for beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,10 @@
 class Beer < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :abv
+  validates_presence_of :description
+  validates_presence_of :food
+  validates_presence_of :image
+
   include PgSearch::Model
   pg_search_scope :search_by_keyword,
                   against: %i[name description],
